d05b76352bd95594e458fd7f47da536c9b238ca0
Sam Hlatywayo <sam.hlatywayo@hotmail.com>
2021-04-13 05:25:41 +0200

    Widen input fields. Setting timezone using .timeZome option

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -4,11 +4,13 @@ import Calculator from './Calculator/Calculator'
 import { logo } from '../svgIcons'
 
 const App = () => {
-  const timezone = Intl.DateTimeFormat().resolvedOptions().locale
+  const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone
   const [isUnitedKingdom, setUnitedKingdom] = useState(false)
 
+  console.log('timezone', timezone)
+
   const getTimeZone = () => {
-    timezone === 'en-GB' && setUnitedKingdom(true)
+    timezone.toLocaleLowerCase() === 'europe/london' && setUnitedKingdom(true)
   }
 
   useEffect(() => {
